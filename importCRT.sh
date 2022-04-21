e68d31236fd0368ad38963670d25479e86a28483
Hugo MOMPHILE <momph@gmail.com>
2022-04-21 15:02:29 -0400

    ajout code script importCRT.sh

--- importCRT.sh
+++ importCRT.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
 # Ce script se charge d'importer le certificat fourni par la PNCN dans le keystore 
+# paramètre du script le nom du fichier .pem à intégrer
+
+read -p "Entrer le nom du keystore : " keystore
+read -p "Entrez le mot de passe du keystore pour intégrer le certificat : " mdp
+alias=${keystore%????}
+if [[ -f $keystore ]] 
+then
+echo "keytool -import -alias $alias -keystore $keystore -file $1 -storepass $mdp"
+else
+echo "keystore non présent"
+fi
